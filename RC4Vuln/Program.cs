9a1e3cc80c96dd14dcb3184856c2b70751976f3e
fisherdog1 <aaronlinnell@gmail.com>
2022-12-08 12:30:47 -0600

    remove unfinished pcap input code

--- RC4Vuln/Program.cs
+++ RC4Vuln/Program.cs
@@ -55,15 +55,6 @@ namespace RC4Vuln
 
         static void Main(string[] args)
         {
-            // Comment this out if not using / you don't have the file
-            FileStream fs = File.OpenRead("A:/pcaps/csce477-03.cap");
-            PcapParser parser = new PcapParser(fs);
-
-            foreach (WeakPacket packet in parser.GetWeakPackets(0))
-            {
-                Console.WriteLine(packet);
-            }
-
             // 64 bit WEP consists of a 3 byte IV and 5 byte Secret
             int nSK = 5;
 
